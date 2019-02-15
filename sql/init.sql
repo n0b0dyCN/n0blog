05952dcfcfaafe2956a463454dda3238394a9dab
n0b0dy <n0b0dypku@gmail.com>
2019-02-15 17:01:47 +0800

    Fix bug in init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -4,17 +4,17 @@ BEGIN
 IF NOT EXISTS (select 1 from pg_database where datname = 'n0blog_dev')
 THEN
  CREATE DATABASE "n0blog_dev";
+ GRANT ALL PRIVILEGES ON DATABASE n0blog_dev TO n0blog;
 END IF;
 IF NOT EXISTS (select 1 from pg_database where datname = 'n0blog_test')
 THEN
  CREATE DATABASE "n0blog_test";
+ GRANT ALL PRIVILEGES ON DATABASE n0blog_test TO n0blog;
 END IF;
 IF NOT EXISTS (select 1 from pg_database where datname = 'n0blog')
 THEN
  CREATE DATABASE "n0blog";
+ GRANT ALL PRIVILEGES ON DATABASE n0blog TO n0blog;
 END IF;
 END
 $do$;
-GRANT ALL PRIVILEGES ON DATABASE n0blog_dev TO n0blog;
-GRANT ALL PRIVILEGES ON DATABASE n0blog_test TO n0blog;
-GRANT ALL PRIVILEGES ON DATABASE n0blog TO n0blog;
